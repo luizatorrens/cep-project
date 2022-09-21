36150a5f2f6520d778e6caf52ee9fe36839c82d8
luizatorrens <luiza.torrens.2006@gmail.com>
2022-09-21 10:58:47 -0300

    ok

---
+++ cep-project/src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      cep: "",
+      endereco: {},
+    };
+  },
+  methods: {
+    async buscar() {
+      const url = `https://viacep.com.br/ws/${this.cep}/json/`;
+      const res = await axios.get(url);
+      this.endereco = res.data;
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>Buscar CEP</h1>
+  <form @submit.prevent="buscar">
+    <input type="text" v-model="cep" placeholder="Digite o CEP" />
+    <button type="submit">Buscar</button>
+  </form>
+  <form class="end">
+    <label for="rua">Rua</label>
+    <input type="text" v-model="endereco.logradouro" />
+    <label for="num">Número</label>
+    <input type="text" v-model="endereco.numero" />
+    <label for="bairro">Bairro</label>
+    <input type="text" v-model="endereco.bairro" />
+  </form>
+  {{ endereco }}
+</template>
